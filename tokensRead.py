6bbd85542e89c7c2009ef4840792654131fb974f
Catalina-fdzmena <A01197705@itesm.mx>
2022-04-20 12:49:21 -0500

    last updates

---
+++ tokensRead.py
@@ -0,0 +1,61 @@
+
+#assign the operators
+
+#use a dictionary
+
+
+#leer archivo
+file = open("read.py")
+
+operators =  {"=" : "Asignación", '+': 'Suma', '/' : 'División', "^" : 'Potencia', '//' : 'Comentario', '*' : 'Multiplicación', '-' : 'Resta'}
+operators_key = operators.keys()
+
+specialSymbol = {"(" : 'Paréntesis que abre', ")" : 'Paréntesis que cierra' }
+specialSymbol_key = specialSymbol.keys()
+
+data_type = {'int' : 'integer type', 'float': 'Floating point' , 'char' : 'Character type', 'long' : 'long int' }
+data_type_key = data_type.keys()
+
+identifier = { 'a' : 'Variable', 'b' : 'Variable', 'c' : 'Variable' , 'd' : 'Variable' }
+
+identifier_key = identifier.keys()
+
+dataFlag = False
+
+a=file.read()
+
+count = 0 
+program = a.split("\n")
+for line in program:
+    count = count + 1
+    print("line#" , count, "\n" , line)
+
+    #tokens=line.split('')
+    print("Tokens are " , tokens)
+    print("Line#", count, "properties \n")
+    for token in tokens:
+        isInt = True
+        if token in operators_key:
+            print(token, operators[token])
+        if token in data_type_key:
+            print(token, data_type[token])
+        if token in identifier_key:
+            print (token, identifier[token])
+        if token in specialSymbol_key:
+            print (token, specialSymbol[token])
+        
+        if token.isnumeric():
+          try:
+            int(token)
+          except ValueError:
+            float(token)
+            float = {token : "Real"}
+            print (token, float[token])
+            isInt = False
+          if  isInt:
+              integer = {token : "Entero"}
+              print (token, integer[token])
+
+          
+
+    dataFlag=False

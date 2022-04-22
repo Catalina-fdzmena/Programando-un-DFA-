63e6f025306e62260efb8dde784d9737d645cc3d
Catalina-fdzmena <A01197705@itesm.mx>
2022-04-22 17:31:16 -0500

    code working correctly

---
+++ tokenRead_2.py
@@ -0,0 +1,70 @@
+file = open("ejemplo.txt", 'r')
+
+def isInt(num):
+    try:
+        int(num)
+        return True
+    except ValueError:
+        return False
+
+def isOperator(char):
+    operadores = ['+', '-', '*', '/', '%', '^', '=']
+    return (char in operadores)
+
+def isFloat(num):
+    try:
+        float(num)
+    except ValueError:
+        return False
+    return('.' in num)
+
+def isVar(char):
+    try:
+        float(char)
+        return False
+    except ValueError:
+        return True
+
+def isRightParentesis(char):
+    parentesis = ['(']
+    return(char in parentesis)
+
+def isLeftParentesis(char):
+    parentesis2 = [')']
+    return(char in parentesis2)
+
+def isComentario(char):
+    return (char == '//')
+
+def checarCaracteres(lista):
+    for char in lista:
+        if isComentario(char):
+            index = lista.index(char)
+            print('Comentario : //')
+            for i in range(index+1, len(lista)):
+                print(lista[i])
+            break
+        elif isRightParentesis(char):
+            print(('Right Parenthesis: ('))
+        elif isLeftParentesis(char):
+            print(('Left Parenthesis: )'))
+        elif isOperator(char):
+            print('Operator : ' + char)
+        elif isVar(char):
+            print('Variable : ' + char)
+        elif isFloat(char):
+            print('Float : ' + char)
+        elif isInt(char):
+            print('Integer : ' + char)
+
+lineas = file.readlines()
+
+caracteres = []
+
+for i in range(len(lineas)):
+   caracteres.append(lineas[i].split())
+   
+print(caracteres)
+
+for i in caracteres:
+    checarCaracteres(i)

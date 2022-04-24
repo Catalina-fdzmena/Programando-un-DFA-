3a5cc3bfd774ff40855f5e66841a6c86a0c6b194
Catalina-fdzmena <A01197705@itesm.mx>
2022-04-23 22:04:03 -0500

    useless code

--- tokensRead.py
+++ tokensRead.py
@@ -7,7 +7,7 @@ Ana Paula Pedroza Ramírez           A00830553
 Andrea Catalina Fernández Mena      A01197705
 Thomas Freund Paternostro           A00831997
 
-"""
+
 
 file = open("ejemplo.txt", 'r')
 
@@ -112,7 +112,7 @@ def lexerAritmetico(lineas):
 lexerAritmetico(lineas)
 
 
-
+"""
 
 
 
